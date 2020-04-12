b58883588435378815fff8f98d6c43501c683131
Jose A <58750343+jose9800@users.noreply.github.com>
2020-04-12 16:51:22 -0700

    Medoto 'use' agregado
    Se agrego el método use para seleccionar la base de datos, adicionalmente se agrego la clase Conexion

---
+++ ABD_DML_Proyecto_Equipo-2/ABD_MDL_Proyecto_Equipo2/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ABD_MDL_Proyecto_Equipo2
+{
+    class Conexion
+    {
+        private SqlConnection conexion = new SqlConnection(@"");    //Ingresa el String de tu conexion a SQL Server
+        public SqlConnection AbrirConexion()    //Abrir conexion
+        {
+            conexion.Open();
+            return conexion;
+        }
+        public SqlConnection CerrarConexion()   //Cerrar conexion
+        {
+            conexion.Close();
+            return conexion;
+        }
+    }
+}
---
+++ ABD_DML_Proyecto_Equipo-2/ABD_MDL_Proyecto_Equipo2/Errores.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ABD_MDL_Proyecto_Equipo2
+{
+    class Errores
+    {
+
+        public void cat(SqlException c)
+        {
+            SqlException e = c;
+            if (e.Number == 208)
+            {
+                Console.WriteLine("Nombre de Tabla no existe");
+                return;
+            }
+            if (e.Number == 207)
+            {
+                Console.WriteLine("Nombre de Columna no existe");
+                return;
+            }
+            if (e.Number == 102)
+            {
+                Console.WriteLine("Error de Sintaxis, verifique su entrada" + "\r\n" + "Ejemplo: insertar en <tabla> columna(s) valores ('valor1', ...)  ");
+
+                return;
+            }
+            if (e.Number == 245)
+            {
+                Console.WriteLine("\r\n  Algun valor introducido no es del tipo correcto; mas detalles: \r\n ");
+
+                Console.WriteLine(e);
+
+                return;
+            }
+
+            else
+            {
+
+                Console.WriteLine("Error en operacion SQL" + e);
+            }
+
+        }
+
+
+    }
+}
